2bfd8cbab7b5c44f0b701a564404b1e64914b162
jarora <jarora@ureeqa.com>
2023-01-31 16:07:04 -0500

    Initial program

---
+++ main.py
@@ -0,0 +1,10 @@
+def pyramid(n):
+    for i in range(n):
+        for j in range(n - i - 1):
+            print(" ", end="")
+        for k in range(2 * i + 1):
+            print("*", end="")
+        print("")
+
+rows = int(input("Enter the number of rows: "))
+pyramid(rows)
